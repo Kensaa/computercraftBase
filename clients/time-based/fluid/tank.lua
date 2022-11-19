bcff416faf622f35917627fab50769f8e5e02b0f
Kensaa <kensadevs@gmail.com>
2022-11-19 12:36:25 +0100

    removed "name" key from fluid client

--- clients/time-based/fluid/tank.lua
+++ clients/time-based/fluid/tank.lua
@@ -8,7 +8,7 @@ end
 local clientType = "time-based grapher"
 local clientName = "Tank 1"
 
-local dataType = '{"type":"fluid storage","unit":"mb","keys":["storage","capacity","name"]}'
+local dataType = '{"type":"fluid storage","unit":"mb","keys":["storage","capacity"]}'
 
 local registerMsg = '{"action":"register","payload":{"id":"'..clientName..'","clientType":"'..clientType..'","dataType":'..dataType..'}}'
 ws.send(registerMsg)
@@ -19,8 +19,7 @@ local tank = peripheral.wrap('back')
 while true do
     local storage = tank.getStored().amount
     local capacity = tank.getCapacity()
-    local name = tank.getStored().name
 
-    ws.send('{"action":"data","payload":{"data":{"storage":'..storage..',"capacity":'..capacity..',"name":"'..name..'"}}}')
+    ws.send('{"action":"data","payload":{"data":{"storage":'..storage..',"capacity":'..capacity..'}}}')
     sleep(1)
 end
